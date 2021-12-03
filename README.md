15c8a34deaeacba5ad0a20ea3bb17627836c7df4
Waed Hafitha <waed@kiitos-tech.com>
2021-12-03 11:56:28 +0200

    documentation

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# guess-the-password
+# Guess the password: [App][1]
+
+<br/>
+
+## By: Waed Hafitha.
+
+<br/>
+
+## About Guess the password app:
+Simple browser game with Vanilla Javascript and JQuery.
+
+<br/>
+
+
+## How: 
+ - Clone the repo 
+ 1. In treminal, type:
+
+ ```
+ - git clone https://github.com/waed-hamza/guess-the-password.git
+ - cd guess-tha-password
+ - code .
+ ```
+
+ 2. in VS code :
+ **Go Live**
+
+<br><br>
+
+## GitHub Pages:
+Visit [Guess the password app][1] game, and review it.
+
+<br><br>
+
+
+## Languages And Tools:
+<br/>
+
+* <img align="left" alt="Visual Studio Code" width="40px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/visual-studio-code/visual-studio-code.png" /> 
+
+<br/>
+
+    Visual Studio Code
+
+<br/> 
+
+* <img align="left" alt="Visual Studio Code" width="50px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png" /> 
+
+<br/>
+
+    HTML/ HTML5
+
+<br/> 
+
+* <img align="left" alt="Visual Studio Code" width="50px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png" /> 
+
+<br/>
+
+    CSS
+
+<br/> 
+
+* <img align="left" alt="Visual Studio Code" width="50px" src="https://raw.githubusercontent.com/github/explore/78df643247d429f6cc873026c0622819ad797942/topics/github/github.png" /> 
+
+<br/>
+
+    GitHub
+
+<br/>
+
+* <img align="left" alt="JavaScript" width="50px" src="https://cdn.iconscout.com/icon/free/png-256/javascript-2038874-1720087.png"/> 
+
+<br/>
+
+    JavaScript & JQuery
+
+<br/>
+
+[1]:https://www.free-css.com/free-css-templates/page257/evolo
--- script.js
+++ script.js
@@ -1,194 +1,194 @@
 // ------ JavaScript Code -------
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     let numberOfWords = 10;
-//     let guessCount = 4;
-//     let password = '';
-
-//     let start = document.getElementById('start');
-
-//     start.addEventListener('click', () => {
-//         toggleClass(document.getElementById('start-screen'), 'hide', 'show');
-//         toggleClass(document.getElementById('game-screen'), 'hide', 'show');
-
-//         startGame();
-//     });
-
-
-//     function toggleClass(element, ...classNames) {
-//         classNames.forEach(name => element.classList.toggle(name));
-//     }
-
-
-//     function startGame() {
-//         let wordsList = document.getElementById('word-list');
-
-//         let randomWords = getRandomValues(words, numberOfWords);
-        
-//         randomWords.forEach(word => {
-//             let li = document.createElement('li');
-//             li.innerText = word;
-//             wordsList.appendChild(li);
-//         });
-
-//         password = getRandomValues(randomWords, 1)[0];
-//         // console.log(password);
-//         setGuessCount(guessCount);
-
-//         wordsList.addEventListener('click', updateGame);
-//     }
-
-
-//     function getRandomValues(array, count) {
-//         return shuffle(array).slice(0, count);
-//     }
-
-//     function shuffle(array) {
-//         let copiedArr = array.slice();
-
-//         for (let idx1 = copiedArr.length - 1; idx1 > 0; idx1--){
-//             let idx2 = Math.floor(Math.random() * (idx1 + 1));
-
-//             // swap idx1 and idx2
-//             [copiedArr[idx1], copiedArr[idx2]] = [copiedArr[idx2], copiedArr[idx1]];
-//         }
-
-//         return copiedArr;
-//     }
-
-//     function setGuessCount(newCount) {
-//         guessCount = newCount;
-//         document.getElementById('guesses-remaining').innerText =
-//             `Guesses remaining ${guessCount}.`;
-//     }
-
-
-//     function updateGame(e) {
-//         if (e.target.tagName === 'LI' && !e.target.classList.contains('disabled')) {
-//             // grab guessed word, check it against password, update view
-//             let guess = e.target.innerText;
-//             let similarityScore = compareWords(guess, password);
-
-//             e.target.classList.add('disabled');
-//             e.target.innerText = `${guess} ---> Matching Letters: ${similarityScore}`;
-
-//             setGuessCount(guessCount - 1);
-
-//             // check whether the game is over
-//             if (similarityScore === password.length) {
-//                 toggleClass(document.getElementById('winner'), 'hide', 'show');
-//                 this.removeEventListener('click', updateGame);
-//             } else if (guessCount === 0) {
-//                 toggleClass(document.getElementById('loser'), 'hide', 'show');
-//                 this.removeEventListener('click', updateGame);
-//             }
-//         }
-//     }
-
-
-//     function compareWords(word1, word2) {
-//         if (word1.length !== word2.length) {
-//             throw 'Words must have the same length';
-//         }
-//         let count = 0;
-//         for (let i = 0; i < word1.length; i++){
-//             if (word1[i] === word2[i]) {
-//                 count++;
-//             }
-//         }
-
-//         return count;
-//     }
-// });
-
-
-
-// ----- JQuery Code -----
-
-$(document).ready(() => {
+document.addEventListener('DOMContentLoaded', function () {
     let numberOfWords = 10;
     let guessCount = 4;
     let password = '';
 
-    let $start = $('#start');
-    $start.on('click', () => {
-        $('#start-screen').toggleClass('hide show');
-        $('#game-screen').toggleClass('hide show');
+    let start = document.getElementById('start');
+
+    start.addEventListener('click', () => {
+        toggleClass(document.getElementById('start-screen'), 'hide', 'show');
+        toggleClass(document.getElementById('game-screen'), 'hide', 'show');
 
         startGame();
     });
 
-    function startGame() {
-        let randomWords = getRandomValues(words, numberOfWords);
-        let $wordsList = $('#word-list');
 
+    function toggleClass(element, ...classNames) {
+        classNames.forEach(name => element.classList.toggle(name));
+    }
+
+
+    function startGame() {
+        let wordsList = document.getElementById('word-list');
+
+        let randomWords = getRandomValues(words, numberOfWords);
+        
         randomWords.forEach(word => {
-            let $li = $('<li>', { text: word });
-            $wordsList.append($li);
+            let li = document.createElement('li');
+            li.innerText = word;
+            wordsList.appendChild(li);
         });
 
         password = getRandomValues(randomWords, 1)[0];
+        // console.log(password);
         setGuessCount(guessCount);
 
-        $wordsList.on('click', 'li', updateGame);
+        wordsList.addEventListener('click', updateGame);
     }
+
 
     function getRandomValues(array, count) {
         return shuffle(array).slice(0, count);
     }
 
     function shuffle(array) {
-        let copiedArr = [...array];
-        
+        let copiedArr = array.slice();
+
         for (let idx1 = copiedArr.length - 1; idx1 > 0; idx1--){
             let idx2 = Math.floor(Math.random() * (idx1 + 1));
+
+            // swap idx1 and idx2
             [copiedArr[idx1], copiedArr[idx2]] = [copiedArr[idx2], copiedArr[idx1]];
         }
+
         return copiedArr;
     }
 
     function setGuessCount(newCount) {
         guessCount = newCount;
-        $('#guesses-remaining').text(`Guesses remaining ${guessCount}.`);
+        document.getElementById('guesses-remaining').innerText =
+            `Guesses remaining ${guessCount}.`;
     }
 
 
     function updateGame(e) {
-        let $target = $(e.target);
-        let $wordsList = $('#word-list');
-
-        if (!$target.hasClass('disabled')) {
-            let guess = $target.text();
+        if (e.target.tagName === 'LI' && !e.target.classList.contains('disabled')) {
+            // grab guessed word, check it against password, update view
+            let guess = e.target.innerText;
             let similarityScore = compareWords(guess, password);
 
-            $target.addClass('disabled');
-            $target.text(function (i, oldText) {
-                return `${oldText} --> Matching Letters: ${similarityScore}`;
-            });
+            e.target.classList.add('disabled');
+            e.target.innerText = `${guess} ---> Matching Letters: ${similarityScore}`;
 
             setGuessCount(guessCount - 1);
 
-            if (similarityScore == password.length) {
-                $('#winner').toggleClass('hide show');
-                $wordsList.off();
-            } else if (guessCount == 0) {
-                $('#loser').toggleClass('hide show');
-                $wordsList.off();
+            // check whether the game is over
+            if (similarityScore === password.length) {
+                toggleClass(document.getElementById('winner'), 'hide', 'show');
+                this.removeEventListener('click', updateGame);
+            } else if (guessCount === 0) {
+                toggleClass(document.getElementById('loser'), 'hide', 'show');
+                this.removeEventListener('click', updateGame);
             }
         }
     }
+
 
     function compareWords(word1, word2) {
         if (word1.length !== word2.length) {
             throw 'Words must have the same length';
         }
-
         let count = 0;
         for (let i = 0; i < word1.length; i++){
             if (word1[i] === word2[i]) {
-                count += 1;
+                count++;
             }
         }
+
         return count;
     }
 });
+
+
+
+// ----- JQuery Code -----
+
+// $(document).ready(() => {
+//     let numberOfWords = 10;
+//     let guessCount = 4;
+//     let password = '';
+
+//     let $start = $('#start');
+//     $start.on('click', () => {
+//         $('#start-screen').toggleClass('hide show');
+//         $('#game-screen').toggleClass('hide show');
+
+//         startGame();
+//     });
+
+//     function startGame() {
+//         let randomWords = getRandomValues(words, numberOfWords);
+//         let $wordsList = $('#word-list');
+
+//         randomWords.forEach(word => {
+//             let $li = $('<li>', { text: word });
+//             $wordsList.append($li);
+//         });
+
+//         password = getRandomValues(randomWords, 1)[0];
+//         setGuessCount(guessCount);
+
+//         $wordsList.on('click', 'li', updateGame);
+//     }
+
+//     function getRandomValues(array, count) {
+//         return shuffle(array).slice(0, count);
+//     }
+
+//     function shuffle(array) {
+//         let copiedArr = [...array];
+        
+//         for (let idx1 = copiedArr.length - 1; idx1 > 0; idx1--){
+//             let idx2 = Math.floor(Math.random() * (idx1 + 1));
+//             [copiedArr[idx1], copiedArr[idx2]] = [copiedArr[idx2], copiedArr[idx1]];
+//         }
+//         return copiedArr;
+//     }
+
+//     function setGuessCount(newCount) {
+//         guessCount = newCount;
+//         $('#guesses-remaining').text(`Guesses remaining ${guessCount}.`);
+//     }
+
+
+//     function updateGame(e) {
+//         let $target = $(e.target);
+//         let $wordsList = $('#word-list');
+
+//         if (!$target.hasClass('disabled')) {
+//             let guess = $target.text();
+//             let similarityScore = compareWords(guess, password);
+
+//             $target.addClass('disabled');
+//             $target.text(function (i, oldText) {
+//                 return `${oldText} --> Matching Letters: ${similarityScore}`;
+//             });
+
+//             setGuessCount(guessCount - 1);
+
+//             if (similarityScore == password.length) {
+//                 $('#winner').toggleClass('hide show');
+//                 $wordsList.off();
+//             } else if (guessCount == 0) {
+//                 $('#loser').toggleClass('hide show');
+//                 $wordsList.off();
+//             }
+//         }
+//     }
+
+//     function compareWords(word1, word2) {
+//         if (word1.length !== word2.length) {
+//             throw 'Words must have the same length';
+//         }
+
+//         let count = 0;
+//         for (let i = 0; i < word1.length; i++){
+//             if (word1[i] === word2[i]) {
+//                 count += 1;
+//             }
+//         }
+//         return count;
+//     }
+// });
